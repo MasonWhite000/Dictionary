e368709dab87980d72a047b1c43a87d95c169ad4
Mason Carl White <mason.white@stu.jefferson.kyschools.us>
2018-03-19 08:04:58 -0400

    first commit

---
+++ DictionaryWork.py
@@ -0,0 +1,20 @@
+crime_movies = {
+    "The Godfather pt1": "Greatest of all time",
+    
+    "The Godfather pt2": "Not as good as the first but still great",
+    
+    "The Godfather pt3": "An Abomination to cinema",
+    
+    "GoodFellas": "RIP Joe Pesci",
+    
+    "Casino": "RIP Joe Pesci pt2",
+    
+    "Scarface": "Al Pacino's best performance",
+    
+    "Reservoir Dogs": "Tarintino's 2nd best",
+    
+    "Pulp Fiction": "Tarintino's best",
+    }
+    
+for key in crime_movies:
+    print (key + ": " + crime_movies[key])
